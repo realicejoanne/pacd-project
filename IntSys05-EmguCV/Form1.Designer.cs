cd9ba044a3958d0eaf4813d95f8e055e634fa99c
Nezio <nemanjaz1994@gmail.com>
2019-02-06 14:57:22 +0100

    Circle detection; Image loading; Basic layout;

--- IntSys05-EmguCV/Form1.Designer.cs
+++ IntSys05-EmguCV/Form1.Designer.cs
@@ -29,12 +29,106 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgBoxDetected = new Emgu.CV.UI.ImageBox();
+            this.imgBoxOriginal = new Emgu.CV.UI.ImageBox();
+            this.tboxImagePath = new System.Windows.Forms.TextBox();
+            this.btnImagePath = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoxDetected)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoxOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightCyan;
+            this.button1.Location = new System.Drawing.Point(384, 481);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imgBoxDetected
+            // 
+            this.imgBoxDetected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgBoxDetected.Location = new System.Drawing.Point(504, 12);
+            this.imgBoxDetected.Name = "imgBoxDetected";
+            this.imgBoxDetected.Size = new System.Drawing.Size(480, 270);
+            this.imgBoxDetected.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBoxDetected.TabIndex = 2;
+            this.imgBoxDetected.TabStop = false;
+            // 
+            // imgBoxOriginal
+            // 
+            this.imgBoxOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgBoxOriginal.Location = new System.Drawing.Point(12, 12);
+            this.imgBoxOriginal.Name = "imgBoxOriginal";
+            this.imgBoxOriginal.Size = new System.Drawing.Size(486, 270);
+            this.imgBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBoxOriginal.TabIndex = 3;
+            this.imgBoxOriginal.TabStop = false;
+            // 
+            // tboxImagePath
+            // 
+            this.tboxImagePath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.tboxImagePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxImagePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxImagePath.ForeColor = System.Drawing.Color.LightCyan;
+            this.tboxImagePath.Location = new System.Drawing.Point(12, 307);
+            this.tboxImagePath.Name = "tboxImagePath";
+            this.tboxImagePath.Size = new System.Drawing.Size(404, 24);
+            this.tboxImagePath.TabIndex = 18;
+            this.tboxImagePath.Text = "  Load image...";
+            // 
+            // btnImagePath
+            // 
+            this.btnImagePath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnImagePath.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.btnImagePath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImagePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImagePath.ForeColor = System.Drawing.Color.LightCyan;
+            this.btnImagePath.Location = new System.Drawing.Point(435, 301);
+            this.btnImagePath.Name = "btnImagePath";
+            this.btnImagePath.Size = new System.Drawing.Size(39, 30);
+            this.btnImagePath.TabIndex = 19;
+            this.btnImagePath.Text = "...";
+            this.btnImagePath.UseVisualStyleBackColor = false;
+            this.btnImagePath.Click += new System.EventHandler(this.btnImagePath_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(994, 568);
+            this.Controls.Add(this.btnImagePath);
+            this.Controls.Add(this.imgBoxOriginal);
+            this.Controls.Add(this.imgBoxDetected);
+            this.Controls.Add(this.tboxImagePath);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EmguCV";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoxDetected)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBoxOriginal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imgBoxDetected;
+        private Emgu.CV.UI.ImageBox imgBoxOriginal;
+        private System.Windows.Forms.TextBox tboxImagePath;
+        private System.Windows.Forms.Button btnImagePath;
     }
 }
 
--- IntSys05-EmguCV/Form1.cs
+++ IntSys05-EmguCV/Form1.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +16,81 @@ namespace IntSys05_EmguCV
 {
     public partial class Form1 : Form
     {
+        public string imagePath;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // debug paths
+            imagePath = "D://Workbench//Shapes.jpg";
+
+
+            StringBuilder msgBuilder = new StringBuilder("Performance: ");
+
+            //Load the image from file and resize it for display
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(imagePath);//Resize(400, 400, Emgu.CV.CvEnum.Inter.Linear, true);
+
+            //Convert the image to grayscale and filter out the noise
+            UMat uimage = new UMat();
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+            //use image pyr to remove noise
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+
+            //Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+            #region circle detection
+            Stopwatch watch = Stopwatch.StartNew();
+            double cannyThreshold = 180.0;
+            double circleAccumulatorThreshold = 120;
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 2.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 5);
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Hough circles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+
+            #region draw circles
+            Image<Bgr, Byte> circleImage = img.CopyBlank();
+            foreach (CircleF circle in circles)
+                circleImage.Draw(circle, new Bgr(Color.Brown), 2);
+            imgBoxDetected.Image = circleImage;
+            #endregion
+
+            Debug.WriteLine(msgBuilder.ToString());
+        }
+
+        private void btnImagePath_Click_1(object sender, EventArgs e)
+        { // set image path
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files (*.jpg;*.jpeg;*.png;*.bmp;)|*.jpg;*.jpeg;*.png;*.bmp;|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            imagePath = ofd.FileName;
+
+            tboxImagePath.Text = "  " + imagePath;   // update label
+
+            // display image if loaded
+            if (imagePath != "")
+            {
+                try
+                {
+                    Image<Bgr, Byte> img = new Image<Bgr, byte>(imagePath);
+                    imgBoxOriginal.Image = img;
+                }
+                catch
+                {
+                    MessageBox.Show("Could not load the image. Are you sure it was a proper image file?", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+
     }
 }

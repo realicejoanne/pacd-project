6eb643964c254b54d851047a54c1cede03425d3c
Nezio <nemanjaz1994@gmail.com>
2019-02-06 16:27:01 +0100

    Added triangle and box detection; needs tweaking

--- IntSys05-EmguCV/Form1.cs
+++ IntSys05-EmguCV/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.Util;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -30,6 +31,8 @@ namespace IntSys05_EmguCV
             imagePath = "D://Workbench//Shapes.jpg";
 
 
+
+
             StringBuilder msgBuilder = new StringBuilder("Performance: ");
 
             //Load the image from file and resize it for display
@@ -56,13 +59,118 @@ namespace IntSys05_EmguCV
             msgBuilder.Append(String.Format("Hough circles - {0} ms; ", watch.ElapsedMilliseconds));
             #endregion
 
+            #region Canny and edge detection
+            watch.Reset(); watch.Start();
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
 
+            /*
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+            */
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               10, //threshold
+               15, //min Line width
+               1); //gap between lines
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Canny & Hough lines - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Find triangles and rectangles
+            watch.Reset(); watch.Start();
+            List<Triangle2DF> triangleList = new List<Triangle2DF>();
+            List<RotatedRect> boxList = new List<RotatedRect>(); //a box is a rotated rectangle
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        
+                        //CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.01, true);
+
+                        if (CvInvoke.ContourArea(approxContour, false) > 1) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 3) //The contour has 3 vertices, it is a triangle
+                            {
+                                Point[] pts = approxContour.ToArray();
+                                triangleList.Add(new Triangle2DF(
+                                   pts[0],
+                                   pts[1],
+                                   pts[2]
+                                   ));
+                            }
+                            else if (approxContour.Size == 4) //The contour has 4 vertices.
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Triangles & Rectangles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> triangleRectangleImage = img.CopyBlank();
+            foreach (Triangle2DF triangle in triangleList)
+                triangleRectangleImage.Draw(triangle, new Bgr(Color.DarkBlue), 2);
+            foreach (RotatedRect box in boxList)
+                triangleRectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            imgBoxDetected.Image = triangleRectangleImage;
+            #endregion
+
+
+            
             #region draw circles
             Image<Bgr, Byte> circleImage = img.CopyBlank();
             foreach (CircleF circle in circles)
-                circleImage.Draw(circle, new Bgr(Color.Brown), 2);
-            imgBoxDetected.Image = circleImage;
+                triangleRectangleImage.Draw(circle, new Bgr(Color.Brown), 2);
+            imgBoxDetected.Image = triangleRectangleImage;
             #endregion
+
+            #region draw lines
+            Image<Bgr, Byte> lineImage = img.CopyBlank();
+            foreach (LineSegment2D line in lines)
+                triangleRectangleImage.Draw(line, new Bgr(Color.Green), 2);
+            imgBoxDetected.Image = triangleRectangleImage;
+            #endregion
+
 
             Debug.WriteLine(msgBuilder.ToString());
         }
